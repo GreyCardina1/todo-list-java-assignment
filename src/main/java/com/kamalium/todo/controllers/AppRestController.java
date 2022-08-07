9c86de17ffd4edb6ae365abc151f205286105dbc
Roman Dinkevich <roman@kamalium.com>
2022-08-07 17:07:05 +0200

    restcontroller fix

--- src/main/java/com/kamalium/todo/controllers/AppRestController.java
+++ src/main/java/com/kamalium/todo/controllers/AppRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -21,36 +22,45 @@ public class AppRestController {
         return repository.findAll();
     }
 
-    @GetMapping("/add")
+    @PostMapping("/add")
     public @ResponseBody
-    RestResponse addItem(@RequestParam String item_value) {
+    RestResponse addItem(@RequestBody Map<String, String> params) {
+        String item_value = params.containsKey("item_value") ? params.get("item_value") : "";
+        if (item_value.length() == 0) {
+            return new RestResponse("NOT_ADDED", null);
+        }
         Item item = new Item(item_value);
         item.setCreated_date(new Timestamp(System.currentTimeMillis()));
         Item saved = repository.save(item);
-        return new RestResponse("Added", saved);
+        return new RestResponse("ADDED", saved);
     }
 
-    @GetMapping("/update")
+    @PostMapping("/update")
     public @ResponseBody
-    RestResponse updateItem(@RequestParam long id, @RequestParam String item_value) {
-        Item item = new Item(item_value);
+    RestResponse updateItem(@RequestBody Map<String, String> params) {
+        long id = params.containsKey("id") ? Long.valueOf(params.get("id")) : -1;
+        String item_value = params.containsKey("item_value") ? params.get("item_value") : "";
         Optional<Item> dbItem = repository.findById(id);
+        if (dbItem.isEmpty() || (item_value.length() == 0)) {
+            return new RestResponse("NOT_UPDATED", null);
+        }
+        Item item = new Item(item_value);
         item.setId(id);
         item.setCreated_date(dbItem.get().getCreated_date());
         if (!item_value.equals(dbItem.get().getItem_value())) {
             item.setUpdated_date(new Timestamp(System.currentTimeMillis()));
         }
         Item saved = repository.save(item);
-        return new RestResponse("Updated", saved);
+        return new RestResponse("UPDATED", item);
     }
 
-    @GetMapping("/delete")
+    @DeleteMapping("/delete")
     public @ResponseBody
     RestResponse deleteItem(@RequestParam long id) {
         Item item = new Item(id);
         item.setId(id);
         repository.delete(item);
-        return new RestResponse("Deleted",item);
+        return new RestResponse("DELETED", item);
     }
 
 
